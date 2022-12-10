6b1da53fc428459cbc027ad2266723bc51d5f7b4
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-12-10 13:33:00 -0500

    Script that exports data of an RESTFUL API, converts CSV format and saves it to a file

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
-"""Doc"""
+"""Exports tasks information of an employee in csv format"""
+import csv
+import requests
+import sys
 
 
 if __name__ == "__main__":
-   pass
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    all_tk = requests.get(
+        url + "todos", params={'userId': user_id}).json()
+    username = user['username']
+
+    with open(user_id + ".csv", "w") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for t in all_tk:
+            writer.writerow(
+                [user_id, username, t['completed'], t['title']]
+            )
