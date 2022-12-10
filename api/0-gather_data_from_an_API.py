62a42ccbcc768cc90b64c838af3aeb613e7be8d0
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-12-10 13:25:51 -0500

    A Python script that returns info about the TODO list progress of an employee (task 0)

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
-"""Doc"""
+"""Returns todo list progress of an employee"""
+import requests
+import sys
 
 
 if __name__ == "__main__":
-   pass
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    all_tk = requests.get(
+        url + "todos", params={'userId': sys.argv[1]}
+    ).json()
+    completed_tasks = [tk['title'] for tk in all_tk if tk['completed'] is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user['name'], len(completed_tasks), len(all_tk)))
+    for title_task in completed_tasks:
+        print("\t {}".format(title_task))
